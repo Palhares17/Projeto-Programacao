6193f021493f942f94461be75096a2aa1fb62521
Matheus Palhares <79490834+Palhares17@users.noreply.github.com>
2022-02-06 19:06:47 -0300

    correções

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ma-pa/CLionProjects/Projeto
+# Install script for directory: C:/Users/ma-pa/CLionProjects/Projeto-Programacao
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ma-pa/CLionProjects/Projeto/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ma-pa/CLionProjects/Projeto-Programacao/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,55 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
 #define tam 10
 
 int main(){
-    int decisao;
-    int escolhaPlano[tam];
-    char plano[tam];
-    int certeza;
+    int escolhaPlano[tam]; /* vetor para guardar plano */
+    char plano[tam]; /* salva testo dos planos */
     int i = 0;
-    int numMatricula[tam];
-    double dinheiroEmCaixa;
+    int numMatricula[tam]; /* salva o número da matrícula */
+    double dinheiroEmCaixa=0;
 
     int menu;
+    printf("Sou atendente virtual.\n");
+    printf("########## Menu ##########\n");
+    printf("Fazer matricula: (1-sim | 2-nao)\n");
+    scanf("%d", &menu);
 
-    while(1) {
-        printf("Bem-vindo!\n");
-        printf("Gostaria de fazer matricula ?\n");
-        printf("1- Sim / 2- Nao\n");
-        scanf("%d", &decisao);
+    if(menu==1) {
+        while(1) {
+            printf("Bem-vindo!\n");
 
-        if(decisao == 2) {
-            printf("Voce nao quer se matricular. Tem certeza?\n");
-            printf("1- Sim / 2- Nao\n");
-            scanf("%d", &certeza);
-
-            if(certeza == 2){
-                printf("Tudo bem, ate a proxima.\n");
-                break;
-            } else {
-                printf("Que bom, vai continuar com a gente!!!\n");
-            }
-         } else {
             printf("Para continuarmos a matriculo escolha os nossos planos.\n");
 
             printf("\n");
 
             printf("Nossos planos sao: \n");
-            printf("1- Premiun:\n- Pode ir todo dia e todos os horarios.\n- Personal para ajudar seu desenvolvimento fisico.\n- Voce tem direito a comer avontede na cantina da academia.\n- Entre outras regalias.\nPOR APENAS (R$ 390,00\n");
+            printf("1- Premiun:\n- Pode ir todo dia e todos os horarios.\n- Personal para ajudar seu desenvolvimento fisico.\n- Voce tem direito a comer avontede na cantina da academia.\n- Entre outras regalias.\nPOR APENAS (R$ 390,00)\n");
 
             printf("\n");
 
-            printf("2- Platinum:\n- Pode ir das 6:00 as 20:00 .\n- Personal para ajudar seu desenvolvimento fisico.\n- Entre outras regalias.\nPOR APENAS (R$ 200,00\n");
+            printf("2- Platinum:\n- Pode ir das 6:00 as 20:00 .\n- Personal para ajudar seu desenvolvimento fisico).\n- Entre outras regalias.\nPOR APENAS (R$ 200,00)\n");
 
             printf("\n");
 
-            printf("3- Bronzeum: \n- Pode ir apenas 12:00 as 20:00 .\n- Sem personal para ajudar seu desenvolvimento fisico.\nPOR APENAS (R$ 100,00\n ");
+            printf("3- Bronzeum: \n- Pode ir apenas 12:00 as 20:00 .\n- Sem personal para ajudar seu desenvolvimento fisico).\nPOR APENAS (R$ 100,00)\n ");
 
             printf("\n");
 
-            printf("4- Lataum: \n-Tem direito a apenas ir a academia das 8:00 as 15:00.\n POR APENAS (R$ 90,00\n ");
+            printf("4- Lataum: \n-Tem direito a apenas ir a academia das 8:00 as 15:00.\n POR APENAS (R$ 90,00)\n ");
 
             printf("\n");
 
@@ -57,31 +47,31 @@ int main(){
             scanf("%d", &escolhaPlano[i]);
 
             switch(escolhaPlano[i]) {
-                case 1:
-                    strcpy(plano, "Premium");
-                    dinheiroEmCaixa += 390.00;
-                    break;
-                case 2:
-                    strcpy(plano, "Platinum");
-                    dinheiroEmCaixa += 200.00;
-                    break;
-                case 3:
-                    strcpy(plano, "Bronzeum");
-                    dinheiroEmCaixa += 100.00;
-                    break;
-                case 4:
-                    strcpy(plano, "Lataum");
-                    dinheiroEmCaixa += 90.00;
-                    break;
+                    case 1:
+                        strcpy(plano, "Premium");
+                        dinheiroEmCaixa += 390.00;
+                        break;
+                    case 2:
+                        strcpy(plano, "Platinum");
+                        dinheiroEmCaixa += 200.00;
+                        break;
+                    case 3:
+                        strcpy(plano, "Bronzeum");
+                        dinheiroEmCaixa += 100.00;
+                        break;
+                    case 4:
+                        strcpy(plano, "Lataum");
+                        dinheiroEmCaixa += 90.00;
+                        break;
             }
 
             printf("Perfeito, terminanos a sua matricula.\n");
-            numMatricula[i] = i*tam;
+            numMatricula[i] = i;
+            printf("Sua matricula eh %d e seu plano eh %s\n", numMatricula[i], plano);
+            printf("\n");
 
-            printf("Sua matricula eh %d e seu plano eh %s", numMatricula[i], plano);
+            i++;
         }
-        i++;
     }
-
-    printf("O dinheiro em caixa eh de %2f", dinheiroEmCaixa);
 }
+
